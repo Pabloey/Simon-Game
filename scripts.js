e83572e1173c022ec7edff02214d3eaed45b911c
Pablo Yocupicio <Pabloey@yahoo.com>
2021-11-18 13:53:13 -0700

    Computer pushes number into an array

--- scripts.js
+++ scripts.js
@@ -6,11 +6,13 @@ const yellowButton = document.getElementById("yellow-button");
 const greenButton = document.getElementById("green-button");
 const redButton = document.getElementById("red-button");
 const blueButton = document.getElementById("blue-button");
+const testButton = document.getElementById("test-button");
 let scoreDisplay = document.getElementById("score-display");
-let RNG = Math.floor(Math.random() * 4);
+let cpuPicks = [];
+let playerPicks = [];
 
 // Player can click on every button through these Event Listeners.
-let playerChoice = () => {};
+
 yellowButton.addEventListener("click", () => {
   console.log("Clicked Yellow Button");
 });
@@ -28,15 +30,26 @@ blueButton.addEventListener("click", () => {
 });
 
 //Computer pulls random number from RNG to choose color
+//Computer pushes a number to array cpuPicks
 let computerChoice = () => {
+  let RNG = Math.floor(Math.random() * 4);
   if (RNG === 0) {
     console.log("Yellow Clicked");
+    cpuPicks.push(0);
   } else if (RNG === 1) {
     console.log("Green Clicked");
+    cpuPicks.push(1);
   } else if (RNG === 2) {
     console.log("Red Clicked");
+    cpuPicks.push(2);
   } else if (RNG === 3) {
     console.log("Blue Clicked");
+    cpuPicks.push(3);
   }
 };
-console.log(computerChoice());
+
+testButton.addEventListener("click", () => {
+  computerChoice();
+});
+
+console.log(cpuPicks);
