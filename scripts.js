01798739327239575cd8e424e1a85b4bf853ba4c
Pablo Yocupicio <Pabloey@yahoo.com>
2021-11-18 13:39:26 -0700

    Computer can now choose random color

--- scripts.js
+++ scripts.js
@@ -1,24 +1,42 @@
 //Smoke test
 console.log("hello, javascript still working");
 
+// Main components of game DOM
 const yellowButton = document.getElementById("yellow-button");
 const greenButton = document.getElementById("green-button");
 const redButton = document.getElementById("red-button");
 const blueButton = document.getElementById("blue-button");
-let scoreDisplay = document.getElementById("score-display")
+let scoreDisplay = document.getElementById("score-display");
+let RNG = Math.floor(Math.random() * 4);
 
+// Player can click on every button through these Event Listeners.
+let playerChoice = () => {};
 yellowButton.addEventListener("click", () => {
-  console.log("Clicked Yellow Button")
-} )
+  console.log("Clicked Yellow Button");
+});
 
 greenButton.addEventListener("click", () => {
-  console.log("Clicked Green Button")
-} )
+  console.log("Clicked Green Button");
+});
 
 redButton.addEventListener("click", () => {
-  console.log("Clicked Red Button")
-} )
+  console.log("Clicked Red Button");
+});
 
 blueButton.addEventListener("click", () => {
-  console.log("Clicked blue Button")
-} )
+  console.log("Clicked blue Button");
+});
+
+//Computer pulls random number from RNG to choose color
+let computerChoice = () => {
+  if (RNG === 0) {
+    console.log("Yellow Clicked");
+  } else if (RNG === 1) {
+    console.log("Green Clicked");
+  } else if (RNG === 2) {
+    console.log("Red Clicked");
+  } else if (RNG === 3) {
+    console.log("Blue Clicked");
+  }
+};
+console.log(computerChoice());
