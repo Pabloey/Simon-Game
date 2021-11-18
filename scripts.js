24f6aa3bee054dd348dc3df79cb7292fcca2e339
Pablo Yocupicio <Pabloey@yahoo.com>
2021-11-18 13:15:09 -0700

    DOM elements selected, player can click buttons and have feedback

--- scripts.js
+++ scripts.js
@@ -1,2 +1,24 @@
 //Smoke test
-console.log('hello');
+console.log("hello, javascript still working");
+
+const yellowButton = document.getElementById("yellow-button");
+const greenButton = document.getElementById("green-button");
+const redButton = document.getElementById("red-button");
+const blueButton = document.getElementById("blue-button");
+let scoreDisplay = document.getElementById("score-display")
+
+yellowButton.addEventListener("click", () => {
+  console.log("Clicked Yellow Button")
+} )
+
+greenButton.addEventListener("click", () => {
+  console.log("Clicked Green Button")
+} )
+
+redButton.addEventListener("click", () => {
+  console.log("Clicked Red Button")
+} )
+
+blueButton.addEventListener("click", () => {
+  console.log("Clicked blue Button")
+} )
