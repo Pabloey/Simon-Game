ecd568d7c1976c3405928f52855380a2ecdc8511
Pablo Yocupicio <Pabloey@yahoo.com>
2021-11-19 14:48:46 -0700

    Loops through array from start the amount of times depending on round.

--- scripts.js
+++ scripts.js
@@ -8,73 +8,108 @@ const redButton = document.getElementById("red-button");
 const blueButton = document.getElementById("blue-button");
 const testButton = document.getElementById("test-button");
 const startButton = document.getElementById("start-button");
-const loseDisplay = document.getElementById("lose-display");
+const textDisplay = document.getElementById("text-display");
 let scoreDisplay = document.getElementById("score-display");
-let score = 0;
+
+let score = 4;
 let cpuPicks = [];
 let playerPicks = [];
 let turn = false;
+let winOrLose = true;
+let loopTimes = 0;
+let loopStop;
 
-//Computer pulls random number from RNG to choose color
 //Computer pushes a number to array cpuPicks[]
-
-console.log(cpuPicks);
+// Need to loop through numbers everytime. Make a loop that reads from start of Array and stops when it reaches playerPicks.length + 1
 let computerChoice = () => {
   if (turn === false) {
-    if (cpuPicks[score] === 0) {
-      console.log("Yellow Clicked");
-      scoreDisplay.innerHTML = "Yellow Clicked";
-    } else if (cpuPicks[score] === 1) {
-      console.log("Green Clicked");
-    } else if (cpuPicks[score] === 2) {
-      console.log("Red Clicked");
-    } else if (cpuPicks[score] === 3) {
-      console.log("Blue Clicked");
-    } else {
-      console.log("Not your turn");
-    }
-    console.log(cpuPicks);
+    loopTimes = 0;
+    score++;
+    scoreDisplay.innerText = score;
+    loopStop = setInterval(() => {
+      if (loopTimes < score) {
+        if (cpuPicks[loopTimes] === 0) {
+          console.log("Green Clicked");
+        }
+        if (cpuPicks[loopTimes] === 1) {
+          console.log("Yellow Clicked");
+        }
+        if (cpuPicks[loopTimes] === 2) {
+          console.log("Red Clicked");
+        }
+        if (cpuPicks[loopTimes] === 3) {
+          console.log("Blue Clicked");
+        } else if (loopTimes === score) {
+          console.log("Players Turn");
+        }
+        loopTimes++;
+      }
+    }, 1000);
   }
+  turn = true;
+  console.log(cpuPicks);
+  console.log("Score number: " + score);
+  console.log("Loop number " + loopTimes);
 };
 
 //Player Functions
-
+// Three condtions to set
+// If player guesses correctly, but still not at last round. Keep going
+// If player guess correctly and array is equal to round amount. Win
+// If player guesses incorretly, reset game to 0, clear arrays.
+let testChoice = () => {
+  if (playerPicks.length === cpuPicks.length) {
+    console.log("Good job, you win");
+  }
+};
+console.log(playerPicks);
 // Event Listeners
 
 // Player can click on every button through these Event Listeners.
-yellowButton.addEventListener("click", () => {
-  playerPicks.push(0);
-});
 greenButton.addEventListener("click", () => {
-  playerPicks.push(1);
+  if (turn === true) {
+    playerPicks.push(0);
+    testChoice();
+  }
+});
+yellowButton.addEventListener("click", () => {
+  if (turn === true) {
+    playerPicks.push(1);
+    testChoice();
+  }
 });
 redButton.addEventListener("click", () => {
-  playerPicks.push(2);
+  if (turn === true) {
+    playerPicks.push(2);
+    testChoice();
+  }
 });
 blueButton.addEventListener("click", () => {
-  playerPicks.push(3);
+  if (turn === true) {
+    playerPicks.push(3);
+    testChoice();
+  }
 });
 
 //Temporary CPU button
 testButton.addEventListener("click", () => {
-  score++;
   // Make run after so many seconds. Don't pass after score turn..
   computerChoice();
 });
 
-function playGame() {
-  score++;
+// Play Game randomizes 21 numbers in cpuPicks array, runs computerChoice.
+let playGame = () => {
   for (let i = 0; i < 21; i++) {
     let RNG = Math.floor(Math.random() * 4);
     cpuPicks.push(RNG);
   }
   computerChoice();
-}
+};
 
 startButton.addEventListener("click", () => {
   cpuPicks = [];
+  playerPicks = [];
   turn = false;
+  score = 4;
   playGame();
 });
-
-console.log(score);
