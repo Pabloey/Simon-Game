4590a6e51a99bbc29c0b90131be00c4211f81952
Pablo Yocupicio <Pabloey@yahoo.com>
2021-11-19 08:38:51 -0700

    Changed logic, make random array start of game, set a boolean to false and flip when player turn, vice-versa

--- scripts.js
+++ scripts.js
@@ -10,54 +10,71 @@ const testButton = document.getElementById("test-button");
 const startButton = document.getElementById("start-button");
 const loseDisplay = document.getElementById("lose-display");
 let scoreDisplay = document.getElementById("score-display");
+let score = 0;
 let cpuPicks = [];
 let playerPicks = [];
-
-// Player can click on every button through these Event Listeners.
-//Player clicks register into an array, playerPicks[]
-
-yellowButton.addEventListener("click", () => {
-  console.log("Clicked Yellow Button");
-  playerPicks.push(0);
-});
-
-greenButton.addEventListener("click", () => {
-  console.log("Clicked Green Button");
-  playerPicks.push(1);
-});
-
-redButton.addEventListener("click", () => {
-  console.log("Clicked Red Button");
-  playerPicks.push(2);
-});
-
-blueButton.addEventListener("click", () => {
-  console.log("Clicked blue Button");
-  playerPicks.push(3);
-});
+let turn = false;
 
 //Computer pulls random number from RNG to choose color
 //Computer pushes a number to array cpuPicks[]
+
+console.log(cpuPicks);
 let computerChoice = () => {
-  let RNG = Math.floor(Math.random() * 4);
-  if (RNG === 0) {
-    console.log("Yellow Clicked");
-    cpuPicks.push(0);
-  } else if (RNG === 1) {
-    console.log("Green Clicked");
-    cpuPicks.push(1);
-  } else if (RNG === 2) {
-    console.log("Red Clicked");
-    cpuPicks.push(2);
-  } else if (RNG === 3) {
-    console.log("Blue Clicked");
-    cpuPicks.push(3);
+  if (turn === false) {
+    if (cpuPicks[score] === 0) {
+      console.log("Yellow Clicked");
+      scoreDisplay.innerHTML = "Yellow Clicked";
+    } else if (cpuPicks[score] === 1) {
+      console.log("Green Clicked");
+    } else if (cpuPicks[score] === 2) {
+      console.log("Red Clicked");
+    } else if (cpuPicks[score] === 3) {
+      console.log("Blue Clicked");
+    } else {
+      console.log("Not your turn");
+    }
+    console.log(cpuPicks);
   }
 };
 
+//Player Functions
 
+// Event Listeners
+
+// Player can click on every button through these Event Listeners.
+yellowButton.addEventListener("click", () => {
+  playerPicks.push(0);
+});
+greenButton.addEventListener("click", () => {
+  playerPicks.push(1);
+});
+redButton.addEventListener("click", () => {
+  playerPicks.push(2);
+});
+blueButton.addEventListener("click", () => {
+  playerPicks.push(3);
+});
+
+//Temporary CPU button
 testButton.addEventListener("click", () => {
+  score++;
+  // Make run after so many seconds. Don't pass after score turn..
   computerChoice();
 });
 
-console.log(cpuPicks);
+function playGame() {
+  score++;
+  for (let i = 0; i < 21; i++) {
+    let RNG = Math.floor(Math.random() * 4);
+    cpuPicks.push(RNG);
+  }
+  computerChoice();
+}
+
+startButton.addEventListener("click", () => {
+  cpuPicks = [];
+  turn = false;
+  playGame();
+});
+
+console.log(score);
